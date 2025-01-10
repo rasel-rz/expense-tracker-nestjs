4080cf75b64f63ef9ae50a9f24af209872bfcfce
Raihan <md.raihanbdrasel@gmail.com>
2025-01-11 03:05:29 +0600

    I am good

--- datasource.config.ts
+++ datasource.config.ts
@@ -2,11 +2,11 @@ import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
 dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASS, DB_NAME, NODE_ENV } = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT, NODE_ENV } = process.env;
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: DB_HOST,
-  port: 5432,
+  port: parseInt(DB_PORT),
   username: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
